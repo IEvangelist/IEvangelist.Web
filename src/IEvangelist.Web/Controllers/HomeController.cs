34f94df95bddc449686ee153e49ac5145e337926
David Pine <david.pine.7@gmail.com>
2017-02-12 14:26:01 -0600

    Angular2 + ASP.NET Core, initial commit

---
+++ src/IEvangelist.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace IEvangelistWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
